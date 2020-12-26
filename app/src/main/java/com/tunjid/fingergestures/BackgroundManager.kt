fcec17366cca3f221460378de059178b3db99814
Adetunji Dahunsi <tjdah100@gmail.com>
2020-12-26 16:51:37 -0500

    Inlined some constants

--- app/src/main/java/com/tunjid/fingergestures/BackgroundManager.kt
+++ app/src/main/java/com/tunjid/fingergestures/BackgroundManager.kt
@@ -33,9 +33,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.os.Parcelable
 import androidx.annotation.IntRange
-import androidx.annotation.StringRes
 import androidx.palette.graphics.Palette
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.tunjid.androidx.core.content.colorAt
 import com.tunjid.androidx.core.delegates.intentExtras
 import com.tunjid.fingergestures.gestureconsumers.GestureConsumer
@@ -50,19 +48,19 @@ import java.util.*
 class BackgroundManager private constructor() {
 
     val backgroundColorPreference: ReactivePreference<Int> = ReactivePreference(
-        preferencesName = BACKGROUND_COLOR,
+        preferencesName = "background color",
         default = App.transformApp { it.colorAt(R.color.colorPrimary) } ?: Color.LTGRAY
     )
     val sliderColorPreference: ReactivePreference<Int> = ReactivePreference(
-        preferencesName = SLIDER_COLOR,
+        preferencesName = "slider color",
         default = App.transformApp { it.colorAt(R.color.colorAccent) } ?: Color.WHITE
     )
     val sliderDurationPreference: ReactivePreference<Int> = ReactivePreference(
-        preferencesName = SLIDER_DURATION,
+        preferencesName = "slider duration",
         default = DEF_SLIDER_DURATION_PERCENT
     )
     val coloredNavPreference: ReactivePreference<Boolean> = ReactivePreference(
-        preferencesName = USES_COLORED_NAV_BAR,
+        preferencesName = "colored nav bar",
         default = Build.VERSION.SDK_INT > Build.VERSION_CODES.P,
         onSet = {
             App.transformApp { it.broadcast(Intent(ACTION_NAV_BAR_CHANGED)) }
@@ -74,10 +72,10 @@ class BackgroundManager private constructor() {
 
     // TODO: Improve this
     val paletteFlowable: Flowable<PaletteStatus> = Flowable.defer {
-        if (!App.hasStoragePermission) return@defer Flowable.just(PaletteStatus.Unavailable(ERROR_NEED_PERMISSION))
+        if (!App.hasStoragePermission) return@defer Flowable.just(PaletteStatus.Unavailable("Need permission"))
 
         val wallpaperManager = App.transformApp { app -> app.getSystemService(WallpaperManager::class.java) }
-            ?: return@defer Flowable.just(PaletteStatus.Unavailable(ERROR_NO_WALLPAPER_MANAGER))
+            ?: return@defer Flowable.just(PaletteStatus.Unavailable("No Wallpaper manager"))
 
         if (isLiveWallpaper(wallpaperManager)) {
             val swatches = getLiveWallpaperWatches(wallpaperManager)
@@ -88,9 +86,9 @@ class BackgroundManager private constructor() {
         }
 
         val drawable = (wallpaperManager.drawable
-            ?: return@defer Flowable.just(PaletteStatus.Unavailable(ERROR_NO_DRAWABLE_FOUND)))
+            ?: return@defer Flowable.just(PaletteStatus.Unavailable("No Drawable found")))
             as? BitmapDrawable
-            ?: return@defer Flowable.just(PaletteStatus.Unavailable(ERROR_NOT_A_BITMAP))
+            ?: return@defer Flowable.just(PaletteStatus.Unavailable("Not a Bitmap"))
 
         val bitmap = drawable.bitmap
         Flowable.fromCallable { Palette.from(bitmap).generate() }
@@ -143,14 +141,6 @@ class BackgroundManager private constructor() {
             intent.action = ACTION_EDIT_WALLPAPER
             return PendingIntent.getBroadcast(app, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         }
-
-    // TODO: Fix this
-    fun setUsesColoredNav(usesColoredNav: Boolean) {
-        App.withApp { app ->
-            app.preferences.edit().putBoolean(USES_COLORED_NAV_BAR, usesColoredNav).apply()
-            app.broadcast(Intent(ACTION_NAV_BAR_CHANGED))
-        }
-    }
 
     fun getSliderDurationText(@IntRange(from = GestureConsumer.ZERO_PERCENT.toLong(), to = GestureConsumer.HUNDRED_PERCENT.toLong())
                               duration: Int): String {
@@ -302,16 +292,6 @@ class BackgroundManager private constructor() {
 
 private const val MAX_SLIDER_DURATION = 5000
 private const val DEF_SLIDER_DURATION_PERCENT = 60
-
-private const val SLIDER_DURATION = "slider duration"
-private const val BACKGROUND_COLOR = "background color"
-private const val SLIDER_COLOR = "slider color"
-private const val USES_COLORED_NAV_BAR = "colored nav bar"
-
-private const val ERROR_NEED_PERMISSION = "Need permission"
-private const val ERROR_NO_WALLPAPER_MANAGER = "No Wallpaper manager"
-private const val ERROR_NO_DRAWABLE_FOUND = "No Drawable found"
-private const val ERROR_NOT_A_BITMAP = "Not a Bitmap"
 
 private const val EXTRA_CHOSEN_COMPONENT = "android.intent.extra.CHOSEN_COMPONENT"
 private const val ACTION_CHANGE_WALLPAPER = "com.tunjid.fingergestures.action.changeWallpaper"
